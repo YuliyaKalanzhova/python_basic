53e54696c1f89fb3400e9affb0a64832b3585aa3
Yuliya Kalanzhova <yuliya.bg22@gmail.com>
2023-10-28 16:21:18 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,4 +1,16 @@
 def greet_user():
     print("Hello")
 
-greet_user
+greet_user()
+
+def greet_user(user_name):
+    print(f"Hello, {user_name}")
+greet_user('Emily')
+
+def describe_zodia(zodia, bunch):
+    print(f"{zodia.title()} is {bunch} bunch")
+    print(f"Zodiz {zodia.title()} is symbolized as {bunch} bunch")
+    print(f"{zodia.title()} is one of the zodiz signs")
+
+describe_zodia('cancer', 'water')
+describe_zodia('libra', 'air')
